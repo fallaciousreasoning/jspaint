049bfb927aa530aecccc4032c3a3a6af1970e653
Jay Harris <jay.harris@outlook.co.nz>
2019-11-08 14:01:58 +1100

    Updates JS Paint to use the new file handling API

--- src/file-handling.js
+++ src/file-handling.js
@@ -1,22 +1,21 @@
 import { fileFromHandle } from './nativefs.js';
 
-// On load, check if we were launched with any file handles. If so, open the
+// Check if we were launched with any file handles. If so, open the
 // first one for editing.
-window.addEventListener('load', async () => {
-    // Check the file handling API is supported.
-    if (!window.launchParams || !window.launchParams.files)
-        return;
+if ('launchQueue' in window) {
+    window.launchQueue.setConsumer(async launchParams => {
+        const handle = launchParams.files && launchParams.files[0];
 
-    const handle = window.launchParams.files[0];
-    // Handle could be undefined if we were launched with no files.
-    if (!handle)
-        return;
-
-    const file = await fileFromHandle(handle);
-    open_from_File(file, error => {
-        if(!error)
+        // Handle could be undefined if we were launched with no files.
+        if (!handle)
             return;
-        
-        show_error_message("Failed to open file:", error);
-    })
-});
+
+        const file = await fileFromHandle(handle);
+        open_from_File(file, error => {
+            if (!error)
+                return;
+
+            show_error_message("Failed to open file:", error);
+        })
+    });
+}
